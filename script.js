ecbe184621d9b8f3010876caccd2f6cc5b4c23ba
Sarabjit Singh <websachin111@gmail.com>
2022-08-15 16:54:36 +0530

    add button to get location

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 console.log("script running");
 
 function getUserLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      // userLocation = position;
+      const { latitude, longitude } = position.coords;
+      let url = `https://us1.locationiq.com/v1/reverse?key=pk.e1d756551237320c6a5c07314adf128d&lat=${latitude}&lon=${longitude}&format=json`;
+      let res = await fetch(url);
+      let data = await res.json();
+      document.getElementById("loader").classList.remove("active");
+      const { country, city, state } = data.address;
 
-  navigator.geolocation.getCurrentPosition(async (position) => {
-    // userLocation = position;
-    const { latitude, longitude } = position.coords;
-    let url = `https://us1.locationiq.com/v1/reverse?key=pk.e1d756551237320c6a5c07314adf128d&lat=${latitude}&lon=${longitude}&format=json`;
-    let res = await fetch(url);
-    let data = await res.json();
-    document.getElementById("loader").classList.remove("active");
-    const { country, town, state } = data.address;
-
+      document.getElementById(
+        "main-location"
+      ).innerHTML = `<h3>${data.display_name}</h3>`;
+      document.getElementById("city").innerText = `${city}`;
+      document.getElementById("country").innerText = `${country}`;
+      document.getElementById("state").innerText = `${state}`;
+    });
+  }else{
     document.getElementById(
       "main-location"
-    ).innerHTML = `<h3>${data.display_name}</h3>`;
-    document.getElementById("city").innerText = `${town}`;
-    document.getElementById("country").innerText = `${country}`;
-    document.getElementById("state").innerText = `${state}`;
-  });
-
+    ).innerHTML = `<h3>Denied</h3>`;
+  }
 }
-getUserLocation();
+// getUserLocation();
 
-window.addEventListener('load', ()=>{
-    document.getElementById("loader").classList.remove("active");
-})
+window.addEventListener("load", () => {
+  document.getElementById("loader").classList.remove("active");
+});
